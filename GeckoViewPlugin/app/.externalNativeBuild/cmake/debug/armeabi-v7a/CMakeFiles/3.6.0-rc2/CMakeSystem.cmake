f1116b28bff0eedf27669602314ebdf4ee0e43e9
Sébastien PERIN <sebastien@vizua3d.com>
2023-08-01 18:16:01 +0200

    chore: build changes due to previious android studio 2022.3.1 migration

--- GeckoViewPlugin/app/.externalNativeBuild/cmake/debug/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ GeckoViewPlugin/app/.externalNativeBuild/cmake/debug/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,13 +1,13 @@
-set(CMAKE_HOST_SYSTEM "Darwin-19.5.0")
-set(CMAKE_HOST_SYSTEM_NAME "Darwin")
-set(CMAKE_HOST_SYSTEM_VERSION "19.5.0")
-set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+set(CMAKE_HOST_SYSTEM "Windows-10.0.19045")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.19045")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("/Users/ianphilips/Library/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+include("C:/Users/sebtr/AppData/Local/Android/Sdk/ndk/15.2.4203891/build/cmake/android.toolchain.cmake")
 
-set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM "Android-24")
 set(CMAKE_SYSTEM_NAME "Android")
-set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_VERSION "24")
 set(CMAKE_SYSTEM_PROCESSOR "armv7-a")
 
 set(CMAKE_CROSSCOMPILING "TRUE")
