071bbcd813a1f9f4045accb4ddcb4bf91d81c6f3
Sébastien PERIN <khelkun@users.noreply.github.com>
2024-02-08 11:41:41 +0100

    feat: read token in local storage per env for console.3dverse.com

--- GeckoViewPlugin/app/src/main/assets/token3dverse/token3dverse.js
+++ GeckoViewPlugin/app/src/main/assets/token3dverse/token3dverse.js
@@ -1,7 +1,15 @@
 let token3dverse = "";
 function fetchToken3dverse()
 {
-    const token = window.apiToken || localStorage.getItem('3dverse-api-token');
+    const LAST_ACCESSED_ENVIRONMENT_ID = "last-accessed-env-id";
+    const ENVIRONMENT_API_TOKEN_PREFIX = "3dverse-api-token-";
+
+    let token = window.apiToken || localStorage.getItem('3dverse-api-token');
+    if(document.location.startsWith('console')) {
+        const environmentId = localStorage.getItem(LAST_ACCESSED_ENVIRONMENT_ID);
+        token = localStorage.getItem(`${ENVIRONMENT_API_TOKEN_PREFIX}${environmentId}`) || token;
+    }
+
     if(!token || token === token3dverse)
     {
         return false;
